f2356ffc3e1884de84932c1e8f75d2fa9c5f3c5b
omeraladen <killerpop1738@gmail.com>
2022-08-27 20:50:44 +0200

    Finsh Main components and navbar ✔

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import {Link , NavLink} from 'react-router-dom'
 import Logo from '../../images/logo.png'
+import {links} from '../../data' 
+import {GoThreeBars} from 'react-icons/go'
 import './navbar.scss'
 
 const Navbar = () => {
@@ -9,7 +11,20 @@ const Navbar = () => {
             <Link to='/' className="logo">
                 <img src={Logo} alt="nav-logo" />
             </Link>
-
+            <ul className='nav__links'>
+              { 
+                links.map( (link , index) => {
+                  return ( 
+                    <li key={index}>
+                      <NavLink to={link.path}>{link.name}</NavLink>
+                    </li>
+                  )
+                } )
+              }
+            </ul>
+              <button className='nav__toggle-btn'>
+                <GoThreeBars/>
+              </button>
 
         </div>
     </nav>
